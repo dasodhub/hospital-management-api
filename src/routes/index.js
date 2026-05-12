72d55d8ca096a6d39de0a9580d5e032678ec06c2
Mustapha Jose <josey7kmt@gmail.com>
2026-05-12 10:24:09 +0100

    chore: merge development into patient module

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth.routes"));
 router.use("/users", require("./user.routes"));
+router.use("/patients", require("./patient.routes"));
 
 module.exports = router;
