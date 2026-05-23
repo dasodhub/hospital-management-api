7d1b467ed20e5f81e8d7d23a544e0d3d9142cc2a
Mustapha Jose <josey7kmt@gmail.com>
2026-05-23 16:40:01 +0100

    fix: add missing routes and fix generateId utility

---
+++ src/utils/generateId.js
@@ -0,0 +1,7 @@
+const generateId = (prefix) => {
+  const timestamp = Date.now().toString();
+  const random = Math.floor(Math.random() * 1000).toString().padStart(3, "0");
+  return `${prefix}-${timestamp}-${random}`;
+};
+
+module.exports = generateId;
